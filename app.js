e992b490833b0b4ded2cbef5d8550c4fa97f4c1c
Laurent Mwema <laumwema733@gmail.com>
2026-07-05 12:05:11 +0930

    Add javascript file and emplement the logic

---
+++ app.js
@@ -0,0 +1,30 @@
+let time = 1500;
+let intervalId;
+
+const timeLabel = document.querySelector(".time");
+console.log(timeLabel);
+
+function createTimer() {
+  const min = String(Math.trunc(time / 60)).padStart(2, 0);
+  const sec = String(time % 60).padStart(2, 0);
+  timeLabel.textContent = `${min}:${sec}`;
+}
+
+function start() {
+  intervalId ??= setInterval(() => {
+    createTimer();
+    time--;
+  }, 1000);
+}
+function stop() {
+  clearInterval(intervalId);
+  intervalId = null;
+}
+function reset() {
+  time = 1500;
+  createTimer();
+}
+
+document.querySelector(".btn-start").addEventListener("click", start);
+document.querySelector(".btn-pause").addEventListener("click", stop);
+document.querySelector(".btn-reset").addEventListener("click", reset);
